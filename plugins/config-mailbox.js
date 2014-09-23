2338f61e9129b5e8bc13f858910fdd236f11bc42
Manuel Araoz <manuelaraoz@gmail.com>
2014-09-23 14:04:09 -0300

    foxtrot integration initial steps

--- plugins/config-mailbox.js
+++ plugins/config-mailbox.js
@@ -1,3 +1,22 @@
 module.exports = {
 
+  identity: '01bad2e8d4cf6c3db4dcc986ea4b2f03195b21afa3f7803d70bee070086d5d3c', // leave emtpy to generate new random identity
+  foxtrot: {
+    discovery: {
+      connect: [],
+      tcpserver: {
+        port: 9333,
+        seek: false,
+      },
+      file: {},
+      dnsseed: {},
+      zeroconf: {
+        discover: true,
+        advertise: true,
+      },
+      rumor: {},
+      reconnect: {},
+    }
+  }
+
 };
--- plugins/mailbox.js
+++ plugins/mailbox.js
@@ -1,23 +1,80 @@
 var microtime = require('microtime');
+var bitcore = require('bitcore');
 var mdb = require('../lib/MessageDb').default();
 var logger = require('../lib/logger').logger;
 var preconditions = require('preconditions').singleton();
+var foxtrot = require('foxtrot');
+var Key = bitcore.Key;
 
 var io;
 module.exports.init = function(ext_io, config) {
   logger.info('Using mailbox plugin');
   preconditions.checkArgument(ext_io);
   io = ext_io;
+
+  foxtrot.options = config.foxtrot;
+  foxtrot.on('peerConnect', function(peer) {
+    console.log('>>> connected to peer (' + peer.descriptorString() + ')');
+  });
+  foxtrot.on('peerDisconnect', function(peer) {
+    console.log('>>> disconnected from peer (' + peer.descriptorString() + ')');
+  });
+
+  var identity = Key.generateSync();
+  if (config.identity) {
+    identity.private = new Buffer(config.identity, 'hex');
+    identity.regenerateSync();
+  }
+
+  var server = foxtrot.createServer({
+    key: identity,
+    advertise: true
+  });
+  console.log('>>> foxtrot server listening on ' + identity.public.toString('hex'));
+
+  server.on('connect', function(socket) {
+    socket.nick = null;
+    socket.clientNumber = chatClients.length;
+    chatClients.push(socket);
+    socket.on('data', function(data) {
+      if (!socket.nick) {
+        socket.nick = data.toString();;
+        for (var i = 0; i < chatClients.length; i++) {
+          var chatClient = chatClients[i];
+          if ((chatClient !== socket) && (chatClient.nick == socket.nick)) {
+            chatClients.splice(chatClient.clientNumber, 1);
+            socket.end('>>> that nickname is already taken\n');
+            return;
+          }
+        }
+        var announcement = '>>> ' + socket.nick + ' has joined the conversation\n';
+        sendToEveryone(announcement, socket);
+      } else {
+        sendToEveryone(socket.nick + '> ' + data, socket);
+      }
+    });
+    socket.on('close', function() {
+      chatClients.splice(socket.clientNumber, 1);
+    });
+  });
+
+
   io.sockets.on('connection', function(socket) {
     // when it requests sync, send him all pending messages
     socket.on('sync', function(ts) {
       logger.verbose('Sync requested by ' + socket.id);
       logger.debug('    from timestamp ' + ts);
+
+      // check that client is subscribed to his public key
       var rooms = socket.rooms;
       if (rooms.length !== 2) {
         socket.emit('insight-error', 'Must subscribe with public key before syncing');
         return;
       }
+
+      // let client know of our foxtrot address
+      socket.emit('foxtrot', identity.public.toString('hex'));
+
       var to = rooms[1];
       var upper_ts = Math.round(microtime.now());
       logger.debug('    to timestamp ' + upper_ts);
@@ -37,7 +94,7 @@ module.exports.init = function(ext_io, config) {
       });
     });
 
-    // when it sends a message, add it to db
+    // when it receives a message, add it to db
     socket.on('message', function(m) {
       logger.debug('Message sent from ' + m.pubkey + ' to ' + m.to);
       mdb.addMessage(m, function(err) {
@@ -52,8 +109,6 @@ module.exports.init = function(ext_io, config) {
   mdb.on('message', broadcastMessage);
 
 };
-
-
 
 var broadcastMessage = module.exports.broadcastMessage = function(message, socket) {
   preconditions.checkState(io);
